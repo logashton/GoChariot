79c8bf354232106f1b9f28f208ff0f35a2c26afe
Logan <logan.ash2025@gmail.com>
2024-03-29 12:14:10 -0400

    setting up source

---
+++ src/test/java/com/gochariot/gochariot/GochariotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gochariot.gochariot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GochariotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
